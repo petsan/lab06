043ce689aa400d16cb1357abefa349ffc12f3a14
Peter Sankiewicz <petsan@hotmail.com>
2020-06-08 15:01:50 -0700

    let's see if this worked -ps

--- server.js
+++ server.js
@@ -10,6 +10,28 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+app.get('/location', (request, response) => {
+  try {
+    let search_query = request.query.city;
+    let geoData  = require('./data/locaton.json');
+    let returnObj = new Location(search_query, geoData[0]);
+
+    console.log(returnObj);
+
+    response.status(200).send(returnObj);
+  } catch(err) {
+    console.log('ERROR', err);
+    response.status(500).send('Server Error');
+  }
+})
+
+function Location(searchQuery, obj) {
+  this.search_query = searchQuery;
+  this.formatted_querry = obj.display_name;
+  this.latitude = obj.lat;
+  this.longitude = obj.lon;
+}
+
 app.get('*', (request, response) => {
   response.status(404).send('This route does not exist')
 });
