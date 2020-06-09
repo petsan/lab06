6f94838b8b9b450f4185964588daa4da8a4d4e1c
Peter Sankiewicz <petsan@hotmail.com>
2020-06-08 17:43:16 -0700

    final checking of lab06, maybe, ps

--- server.js
+++ server.js
@@ -10,32 +10,60 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+let error404 = 'The page does not exist.';
+let error500 = 'Sorry, something went wrong.';
+
+
 app.get('/location', (request, response) => {
   try {
-    console.log(request.query)
     let search_query = request.query.city;
     let geoData  = require('./data/location.json');
     let returnObj = new Location(search_query, geoData[0]);
 
-    console.log(returnObj);
-
     response.status(200).send(returnObj);
   }
   catch(err) {
-    console.log('ERROR', err);
-    response.status(500).send('Server Error');
+    response.status(500).send(error500);
   }
 })
 
-function Location(searchQuery, obj) {
-  this.search_query = searchQuery;
-  this.formatted_querry = obj.display_name;
-  this.latitude = obj.lat;
-  this.longitude = obj.lon;
+app.get('/weather', (request, response) => {
+  try {
+    let search_query = request.query;
+    let weatherArray = [];
+    let weatherData = require('./data/weather.json');
+
+    weatherData.data.forEach(value => {
+      let weatherForecast = new Weather(search_query, value);
+      weatherArray.push(weatherForecast);
+    })
+
+    response.status(200).send(weatherArray);
+  }
+  catch(err) {
+    response.status(500).send(error500);
+  }
+})
+
+class Weather {
+  constructor(searchQuery, obj) {
+    this.search_query = searchQuery;
+    this.forecast = obj.weather.description;
+    this.time = obj.valid_date;
+  }
+}
+
+class Location {
+  constructor(searchQuery, obj) {
+    this.search_query = searchQuery;
+    this.formatted_querry = obj.display_name;
+    this.latitude = obj.lat;
+    this.longitude = obj.lon;
+  }
 }
 
 app.get('*', (request, response) => {
-  response.status(404).send('This route does not exist')
+  response.status(404).send(error404);
 });
 
 app.listen(PORT, () => {
